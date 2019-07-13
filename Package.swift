713f5c987563b04e72405d105e5d2e422a7aa0d3
Jan Kubny <30832483+SmartJSONEditor@users.noreply.github.com>
2019-07-13 10:29:59 -0600

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "AXPhotoViewer",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "AXPhotoViewer", targets: ["AXPhotoViewer"])
+    ],
+    targets: [
+        .target(
+            name: "AXPhotoViewer",
+            path: "Sources"
+        )
+    ]
+)
